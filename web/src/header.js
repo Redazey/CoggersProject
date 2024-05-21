3a67e2dac2b6bcfbd037df1bd0a7cc1a1fb29208
Redazey <w.d.gaster00@mail.ru>
2024-05-21 22:17:54 +0300

    Почти сверстал шапку сайта

--- web/src/header.js
+++ web/src/header.js
@@ -4,15 +4,23 @@ import './header.css';
 const Header = () => {
   return (
     <div className="Header">
-      <nav>
-        <a href="#" className="navHref">Личный кабинет</a>
-        <a href="#" className="navHref">Помощь</a>
-        <a href="#" className="navStyledHref">Играть</a>
-        <a href="#" className="navHref">Магазин</a>
-        <a href="#" className="navStyledHref">Войти</a>
-      </nav>
+        <div className="horizontalNav">
+            <a className="horizontalNav__logo" href="#"><img src={require('./imgs/logo.png')}/></a>
+            <div className="horizontalNav__MainButtons">
+                <a href="#">Играть</a>
+                <a href="#">Войти</a>
+            </div>
+        </div>
+
+        <div className="verticalNav">
+            <ul class="vertical-list">
+                <li><a href="#">Личный кабинет</a></li>
+                <li><a href="#">Помощь</a></li>
+                <li><a href="#">Магазин</a></li>
+            </ul>
+        </div>
     </div>
-  );
+    );
 };
 
 export default Header;
